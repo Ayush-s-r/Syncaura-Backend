42ea6fb847f4b827331124f4aa52d42d86acca50
Ayush-s-r <rawatarvind4565@gmail.com>
2025-12-23 17:06:35 +0530

    Task API added

--- src/server.js
+++ src/server.js
@@ -1,11 +1,24 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import taskRoutes from "./routes/task.routes.js";
+
+dotenv.config();
+
 const app = express();
 
-const PORT = 3000;
+// middleware
+app.use(express.json());
 
+// connect database
+connectDB();
 
+const PORT = process.env.PORT || 3000
+
+// routes
+app.use("/api/tasks", taskRoutes);
 
 app.listen(PORT, () => {
-    console.log(`sever is listening on port ${PORT}`)
+    console.log(`server is listening on port ${PORT}`)
 })
 
