d29f8951c2baee85e74d601677b10f1fbb478612
stitipragyan barik <stitipragyanbarik@email.com>
2025-12-26 18:19:41 +0530

    Added auth module

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,4 +1,8 @@
-module.exports = (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: err.message });
+export const errorMiddleware = (err, req, res, next) => {
+  console.error(err);
+  const status = err.status || 500;
+  res.status(status).json({
+    message: err.message || 'Internal server error',
+    ...(err.details ? { details: err.details } : {})
+  });
 };
